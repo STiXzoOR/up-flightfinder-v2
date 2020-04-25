8286e98aceb2c8fe3f813c7b1e36cda07bd980f6
Neoptolemos Kyriakou <neostixz@gmail.com>
2020-04-25 16:17:21 +0300

    added missing vendors path to gulp.js

--- config/gulp.js
+++ config/gulp.js
@@ -8,62 +8,63 @@ const paths = {
     '!dist/static/fonts',
     '!dist/static/fonts/**/*',
     '!dist/static/vendors',
-    '!dist/static/vendors/**/*'
+    '!dist/static/vendors/**/*',
   ],
+  vendors_dir: 'dist/static/vendors',
   images_dir: 'dist/static/images',
   dist_dir: 'dist',
   dist_files: 'dist/**/*.*',
   views: {
     src: 'src/views/**/*.pug',
-    dist: 'dist/views'
+    dist: 'dist/views',
   },
   css: {
     src: 'src/static/css/**/*.css',
-    dist: 'dist/static/css'
+    dist: 'dist/static/css',
   },
   js: {
     src: 'src/static/js/**/*.js',
-    dist: 'dist/static/js'
+    dist: 'dist/static/js',
   },
   fonts: {
     src: 'src/static/fonts/**/*',
-    dist: 'dist/static/fonts'
+    dist: 'dist/static/fonts',
   },
   images: {
     src: 'src/static/images/**/*.+(png|jpg|jpeg|svg|ico)',
-    dist: 'dist/static/images'
+    dist: 'dist/static/images',
   },
   vendors: {
     fontawesome: {
       src: [
         'node_modules/@fortawesome/fontawesome-free/*css*/all.min.css',
-        'node_modules/@fortawesome/fontawesome-free/*webfonts*/*'
+        'node_modules/@fortawesome/fontawesome-free/*webfonts*/*',
       ],
-      dist: 'dist/static/vendors/font-awesome'
+      dist: 'dist/static/vendors/font-awesome',
     },
     jquery: {
       src: 'node_modules/jquery/dist/jquery.min.js',
-      dist: 'dist/static/vendors/jquery'
+      dist: 'dist/static/vendors/jquery',
     },
     popper: {
       src: 'node_modules/popper.js/dist/umd/popper.min.js',
-      dist: 'dist/static/vendors/popper.js'
+      dist: 'dist/static/vendors/popper.js',
     },
     bootstrap: {
       src: [
         'node_modules/bootstrap/dist/*css*/bootstrap.min.css',
         'node_modules/bootstrap/dist/*css*/bootstrap-reboot.min.css',
-        'node_modules/bootstrap/dist/*js*/bootstrap.min.js'
+        'node_modules/bootstrap/dist/*js*/bootstrap.min.js',
       ],
-      dist: 'dist/static/vendors/bootstrap'
+      dist: 'dist/static/vendors/bootstrap',
     },
     datepicker: {
       src: ['node_modules/flatpickr/dist/flatpickr.min.css', 'node_modules/flatpickr/dist/flatpickr.min.js'],
-      dist: 'dist/static/vendors/flatpickr'
+      dist: 'dist/static/vendors/flatpickr',
     },
     select2: {
       src: ['node_modules/select2/dist/*css*/select2.min.css', 'node_modules/select2/dist/*js*/select2.full.min.js'],
-      dist: 'dist/static/vendors/select2'
+      dist: 'dist/static/vendors/select2',
     },
     select2_bootstrap: {
       src: 'node_modules/select2-theme-bootstrap4/dist/select2-bootstrap.min.css',
@@ -80,11 +81,11 @@ module.exports = {
       port: 5000,
       files: [paths.dist_files],
       browser: 'google chrome',
-      notify: true
+      notify: true,
     },
     nodemon: {
       script: './bin/www',
-      ignore: ['gulpfile.js', 'config/', 'node_modules']
-    }
-  }
+      ignore: ['gulpfile.js', 'config/', 'node_modules'],
+    },
+  },
 };
