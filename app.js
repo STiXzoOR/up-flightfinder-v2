b8990b75f2c083547a6a58c12d3cd78f76885557
Neoptolemos Kyriakou <neostixz@gmail.com>
2020-04-26 13:17:51 +0300

    implemented user authentication with passport

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 
 const express = require('express');
-const expressSession = require('express-session');
+const session = require('express-session');
+const passport = require('passport');
+const uuid = require('uuid/v4');
 const favicon = require('serve-favicon');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const flash = require('express-flash-2');
 const redis = require('redis');
-const RedisStore = require('connect-redis')(expressSession);
+const RedisStore = require('connect-redis')(session);
 
 const indexRouter = require('./src/routes/index');
 const usersRouter = require('./src/routes/users');
@@ -24,22 +25,36 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/dist/static')));
 app.use(favicon(path.join(__dirname, '/dist/static/images', 'favicon.ico')));
 
 app.use(
-  expressSession({
-    secret: 'session',
+  session({
+    genid: (req) => {
+      return uuid();
+    },
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: false,
-    duration: 30 * 60 * 1000,
-    activeDuration: 5 * 6 * 1000,
-    store: new RedisStore({ host: 'localhost', port: 6379, client: redisClient, ttl: 260 })
+    cookie: { secure: false, maxAge: 1800000 },
+    store: new RedisStore({ host: 'localhost', port: 6379, client: redisClient, ttl: 260 }),
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
+
+app.use((req, res, next) => {
+  if (req.session.user === undefined) {
+    req.session.user = { id: 0, isAuthenticated: false, role: 'GUEST' };
+  }
+
+  if (res.locals.user === undefined) {
+    res.locals.user = req.session.user;
+  }
+
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const createError = require('http-errors');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const mysql = new (require('../config/mysql'))();
+
+require('../config/passport')(passport);
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+  next(createError(400));
 });
 
 router.get('/sign-in', (req, res, next) => {
@@ -13,6 +19,90 @@ router.get('/sign-in', (req, res, next) => {
 
 router.get('/sign-up', (req, res, next) => {
   res.render('sign-up');
+});
+
+router.get('/sign-out', (req, res) => {
+  req.session.destroy(() => {
+    req.logout();
+    res.redirect('/');
+  });
+});
+
+router.post('/sign-in', (req, res, next) => {
+  passport.authenticate('login', (err, response) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (response.error) {
+      const error = response.message;
+      return res.render('sign-in', { error });
+    }
+
+    req.login(response.user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.session.user = response.user;
+      req.session.user.isAuthenticated = req.isAuthenticated();
+      return res.redirect('/');
+    });
+  })(req, res, next);
+});
+
+router.post('/sign-up', async (req, res, next) => {
+  const data = req.body;
+  const response = {
+    status: 400,
+    error: true,
+    message: '',
+  };
+
+  const salt = bcrypt.genSaltSync(10);
+  const password = bcrypt.hashSync(data.password, salt);
+
+  const userExistsQuery = 'SELECT email FROM customer WHERE email=:email';
+  const userInsertQuery =
+    'INSERT INTO customer (first_name, last_name, email, password, mobile, gender, joined_date, status, customer_type) VALUES (:firstName, :lastName, :email, :password, :mobile, :gender, NOW(), :status, "USER")';
+
+  await mysql
+    .fetch(userExistsQuery, { email: data.email })
+    .then((result) => {
+      if (result.length !== 0) throw new Error();
+      const fields = {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        password,
+        mobile: data.mobile,
+        gender: data.gender,
+        status: 'CONFIRMED',
+      };
+      return mysql.commit(userInsertQuery, fields);
+    })
+    .catch(() => {
+      response.status = 401;
+      response.error = true;
+      response.message = 'The user already exists. Please sign in.';
+    })
+    .then(() => {
+      if (response.status === 401) return;
+      response.status = 200;
+      response.error = false;
+      response.message = 'Sign up success. Please sign in.';
+    })
+    .catch(() => {
+      response.status = 500;
+      response.error = true;
+      response.message = 'Something went wrong while signing you up. Please try again.';
+    });
+
+  if (response.error) {
+    res.status(response.status).render('sign-up', { error: response.message });
+  } else {
+    res.flash('success', response.message);
+    res.redirect('/user/sign-in');
+  }
 });
 
 module.exports = router;
